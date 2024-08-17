c9dedf9899be8bb256428b44907391fea233a612
ssterings <satyan.stering@gmail.com>
2024-08-17 19:38:56 -0300

    Primeiro commit

---
+++ src/main/java/br/com/satyan/stering/saita/fincancaspessoais/FincancasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.satyan.stering.saita.fincancaspessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincancasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincancasPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/satyan/stering/saita/fincancaspessoais/FincancasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.satyan.stering.saita.fincancaspessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincancasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
